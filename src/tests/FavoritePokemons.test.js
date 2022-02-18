688688334a02a982013beeb3c053c0fe414a8dc7
Léia Ribeiro <leiaribeirot@gmail.com>
2022-02-18 19:27:47 -0400

    Dev. Req. 3 - Testes FavoritePokemons

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,28 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../utils/renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+import data from '../data';
+
+describe('Testando FavoritePokemons.js', () => {
+  test('se é exibida uma mensagem caso a pessoa não tenha pokémon favorito.', () => {
+    renderWithRouter(<FavoritePokemons data={ [] } />);
+
+    const noFound = screen.getByText('No favorite pokemon found');
+
+    expect(noFound).toBeInTheDocument();
+  });
+
+  test('se são exibidos todos os cards de pokémon favoritados.', () => {
+    const idPikachu = 25;
+
+    const favoritePokemon = data.filter(
+      ({ id }) => id === idPikachu,
+    );
+
+    renderWithRouter(<FavoritePokemons pokemons={ favoritePokemon } />);
+
+    const pikachu = screen.getByText('Pikachu');
+    expect(pikachu).toBeInTheDocument();
+  });
+});

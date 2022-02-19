1b528a6cbc9115a3e69f9d8b82fdfe74e98549bd
Léia Ribeiro <leiaribeirot@gmail.com>
2022-02-19 19:11:24 -0400

    Dev. Req.4 - Testes do Componente NotFound.js

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,25 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../utils/renderWithRouter';
+import NotFound from '../components/NotFound';
+
+describe('Testando NotFound.js', () => {
+  test('se página contém um heading h2 com um texto específico.', () => {
+    renderWithRouter(<NotFound />);
+
+    const notFoundText = screen.getByRole('heading',
+      { level: 2, name: /Page requested not found/ });
+
+    expect(notFoundText).toBeInTheDocument();
+  });
+
+  test('se página mostra uma imagem.', () => {
+    renderWithRouter(<NotFound />);
+
+    const notFoundImage = screen.getByAltText(
+      'Pikachu crying because the page requested was not found',
+    );
+
+    expect(notFoundImage.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
